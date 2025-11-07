24066128bf962714f870281b2680306c8dfee027
htamas02 <125739801+htamas02@users.noreply.github.com>
2025-11-07 23:44:25 +0100

    Add files via upload
    ESP32 Kamera micropython kód

---
+++ maincam.py
@@ -0,0 +1,43 @@
+import network
+import socket
+import camera
+import time
+import urequests
+import machine
+
+
+#Wifi csatlakozás
+ssid = 'HEGYINET'
+password = 'Xa47$Pa7#hW4@i6'
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+for _ in range(20):
+    if station.isconnected():
+        break
+    time.sleep(1)
+
+if station.isconnected():
+    print('WiFi csatlakozva:', station.ifconfig())
+else:
+    print('WiFi hiba! Ellenőrizd az SSID-t és jelszót.')
+
+camera.init(0, format=camera.JPEG, fb_location=camera.PSRAM)
+camera.quality(10)
+
+
+while True:
+    time.sleep(5)
+    buf = camera.capture()
+    try:
+        url = "http://192.168.0.37:5000/upload_cam"  # Flask szerver
+        headers = {"Content-Type": "application/octet-stream"}
+        r = urequests.post(url, data=buf, headers=headers)
+        print("Kép elküldve, válasz:", r.text)
+        r.close()
+    except Exception as e:
+        print("Hiba kép küldés közben:", e)
+    #camera.deinit()
+    time.sleep(300)
